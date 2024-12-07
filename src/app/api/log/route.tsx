2833f59ffb01fcb86b99bab19a8c60091ee7a2fa
Gustavo Coimbra <whoisgustavolima@gmail.com>
2024-12-06 23:19:31 -0300

    youthumb

--- src/app/api/log/route.tsx
+++ src/app/api/log/route.tsx
@@ -1,19 +1,21 @@
 import { NextResponse } from "next/server";
 import { PrismaClient } from "@prisma/client";
-
 const prisma = new PrismaClient();
-
 export async function GET(req: Request) {
-    const url = new URL(req.url);
-    const action = `${url.searchParams.get('action')}`;  
-    const forwardedFor = req.headers.get('x-forwarded-for');
-    const ip = forwardedFor ? forwardedFor.split(',')[0] : 'IP não encontrado';
-    const insert = await prisma.log.create({
-        data: {
-            action: action,  
-            ip: ip          
-        }
-    });
-
-    return NextResponse.json(insert);
+    try {
+        const url = new URL(req.url);
+        const action = `${url.searchParams.get('action')}`;
+        const forwardedFor = req.headers.get('x-forwarded-for');
+        const ip = forwardedFor ? forwardedFor.split(',')[0] : 'IP não encontrado';
+        const insert = await prisma.log.create({
+            data: {
+                action: action,
+                ip: ip,
+            }
+        });
+        return NextResponse.json(insert);
+    } catch (error) {
+        console.error("Error creating log:", error);
+        return NextResponse.json({ error: "Something went wrong" }, { status: 500 });
+    }
 }
