63659f70bd0ff52faa34a3e9a1e9bae3c8904058
Gustavo Coimbra <whoisgustavolima@gmail.com>
2024-12-06 23:16:06 -0300

    youthumb

--- src/app/api/log/route.tsx
+++ src/app/api/log/route.tsx
@@ -6,10 +6,8 @@ const prisma = new PrismaClient();
 export async function GET(req: Request) {
     const url = new URL(req.url);
     const action = `${url.searchParams.get('action')}`;  
-    
     const forwardedFor = req.headers.get('x-forwarded-for');
     const ip = forwardedFor ? forwardedFor.split(',')[0] : 'IP não encontrado';
-
     const insert = await prisma.log.create({
         data: {
             action: action,  
