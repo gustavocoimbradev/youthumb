388b2f3d5f1d5ea2d69124e7018ce103e5b2db23
Gustavo Coimbra <whoisgustavolima@gmail.com>
2024-12-11 20:59:36 -0300

    youthumb

--- src/components/Brand/index.tsx
+++ src/components/Brand/index.tsx
@@ -2,7 +2,7 @@ export default function Title() {
 
     return (
         <a href="./" className="text-4xl font-bold">
-          <span className="text-slate-600 text-slate-300">
+          <span className="text-slate-300 text-slate-300">
             YOU<mark className="ms-1 bg-slate-900 text-slate-300 py-2 px-3 rounded-[18px]">THUMB</mark>
           </span>
         </a>
